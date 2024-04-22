f7cc74c418b57c215c417967f9c19c7ad84c25b4
tayoshi-tokisada <mail.by.blacksheep@gmail.com>
2024-04-22 19:45:41 +0900

    lesson ~kadai_008

---
+++ jquery-basic/js/form.js
@@ -0,0 +1,29 @@
+$(function(){
+  $("#check").on("click", function(){
+    console.log("名前：" + $("[name='username']").val());
+    console.log("性別：" + $("[name='gender']").val());
+    console.log("血液型：" + $("[name='blood']").val());
+
+    $("[name='hobby']:checked").each(function(){
+      console.log("趣味：" + $(this).val());
+    });
+  });
+
+  $("[name='username']").on("input", function(){
+    let input = $(this).val();
+
+    if(input){
+      $("#check").prop("disabled", false);
+    }
+    else{
+      $("#check").prop("disabled", true);
+    }
+  });
+
+  $("[name='hobby']").on("change", function(){
+    if($("[name='hobby']:checked").length > 3){
+      $(this).prop("checked", false);
+      alert("3つまでしか選択できません。");
+    }
+  });
+});
