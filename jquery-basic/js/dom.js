5009a31f41e2285aa36236271eb7c2bc7b6fa98c
tayoshi-tokisada <mail.by.blacksheep@gmail.com>
2024-04-22 16:50:25 +0900

    lesson ~kadai_007

---
+++ jquery-basic/js/dom.js
@@ -0,0 +1,30 @@
+$(function(){
+  const p = $("p").text();
+  console.log(p);
+  $("p").text("jQueryを勉強しよう");
+
+  let num = 0;
+
+  $("#append").on("click", function(){
+    num++;
+    $("ul").append("<li>appendで追加" + num + "</li>");
+  });
+
+  $("#remove").on("click", function(){
+    if(num <= 0){
+      num = 0;
+    }
+    else{
+      num--;
+      $("li:last").remove();
+    }
+  });
+
+  $("#show").on("click", function(){
+    $("ul").show();
+  });
+
+  $("#hide").on("click", function(){
+    $("ul").hide();
+  });
+});
